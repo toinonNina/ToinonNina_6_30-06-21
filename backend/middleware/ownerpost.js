13e60061ba9cf05647d60ea2bfa49a37c96e0f3e
nina <zd.toinon@gmail.com>
2021-07-29 20:48:10 +0200

    mise en place middleware sécurité pour postman

--- backend/middleware/ownerpost.js
+++ backend/middleware/ownerpost.js
@@ -7,9 +7,9 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.DB_TOKEN);
     const userId = decodedToken.userId;
-
-    conn.query('SELECT * FROM user WHERE id =?', req.params.id, (error, result) => {
-        if ((result[0].id === userId) || result.isAdmin === 1) {
+    const isAdmin = decodedToken.isAdmin;
+    conn.query('SELECT post.id, user_id FROM post INNER JOIN user ON user.id = post.user_id WHERE post.id=? ', req.params.id, (error, result) => {
+        if ((result[0].user_id === userId) || isAdmin === 1) {
             console.log("action autorisé");
             next();
         } else {
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 // Controle si l'email de l'utilisateur est déja enregistrer
-
+const authcomm = require('../middleware/ownercomment');
 const userCtrl = require('../controllers/comment');
 
 
 
 router.post('/create', auth, userCtrl.createComment);
-router.delete('/delete/:id', auth, userCtrl.deleteComment);
+router.delete('/delete/:id', auth, authcomm, userCtrl.deleteComment);
 router.get('/:id', auth, userCtrl.getAllComm);
+router.get('/onecom/:id', auth, userCtrl.getOneComm);
 
 module.exports = router;
